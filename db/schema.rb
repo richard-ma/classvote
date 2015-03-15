51447efe4f5bdfc02613b7679d2de54e98eb5983
Richard Ma <richard.ma.19850509@gmail.com>
2015-03-15 22:39:17 +0800

    add post_id to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315142851) do
+ActiveRecord::Schema.define(version: 20150315143623) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "post_id"
   end
 
   create_table "posts", force: :cascade do |t|

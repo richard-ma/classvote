43e04f13263b9da60653ec8059dac41533eb3a15
Richard Ma <richard.ma.19850509@gmail.com>
2015-03-15 22:29:38 +0800

    add comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315142523) do
+ActiveRecord::Schema.define(version: 20150315142851) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"

77349d08ee6d246acb37bd39734e6b247d79c2ba
Richard Ma <richard.ma.19850509@gmail.com>
2015-03-15 23:56:29 +0800

    add user

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315154551) do
+ActiveRecord::Schema.define(version: 20150315154812) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "post_id"
+    t.integer  "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -25,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150315154551) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|

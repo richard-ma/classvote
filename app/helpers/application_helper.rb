5a4d604994db1bbef433065ea142c70fa22a2488
Richard Ma <richard.ma.19850509@gmail.com>
2015-03-16 15:53:40 +0800

    get user ip and new user model after user first access this site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def get_number_of_machine()
+    # machine number = last ip address number + 1
+    User.find(session[:user_id]).ip.split('.')[3].to_i + 1
+  end
+
+  def get_ip_of_machine()
+    User.find(session[:user_id[)
+  end
 end

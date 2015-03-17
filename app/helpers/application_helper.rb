75ecf27eea5b07dee135deffa43f98a089b476d2
Richard Ma <richard.ma.19850509@gmail.com>
2015-03-17 08:27:42 +0800

    解决在IE6下不能播放动画的问题

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def get_number_of_machine
     # machine number = last ip address number + 1
-    User.find(session[:user_id]).ip.split('.')[3].to_i + 1
+    User.find(session[:user_id]).ip.split('.')[3].to_i
   end
 
   def get_ip_of_machine
@@ -13,14 +13,14 @@ module ApplicationHelper
     admin_ip = [
       #'127.0.0.1',
       '192.168.1.230',
-      '192.168.1.110',
+      #'192.168.1.110',
     ]
 
     true if admin_ip.include? get_ip_of_machine
   end
 
   def server_ip
-    '127.0.0.1'
+    '192.168.1.230'
   end
 
   def server_port
